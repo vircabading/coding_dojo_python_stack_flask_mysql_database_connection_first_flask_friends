7db72cf5f2dd4d1648ad772454f5abdf66b2c293
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-03 14:45:16 -0700

    add cleaned up mysqlconnection

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -1,42 +1,34 @@
 import pymysql.cursors
+
 class MySQLConnection:
     def __init__(self, db):
-        connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', # change the user and password as needed
-                                    password = 'root', 
-                                    db = db,
-                                    charset = 'utf8mb4',
-                                    cursorclass = pymysql.cursors.DictCursor,
-                                    autocommit = True)
-        self.connection = connection
-    def query_db(self, query, data=None):
+        self.connection = pymysql.connect(
+            host = 'localhost',
+            user = 'root', 
+            password = 'root',#YOUR PASSWORD HERE INSTEAD OF 'root'
+            db = db,
+            charset = 'utf8mb4',
+            cursorclass = pymysql.cursors.DictCursor,
+            autocommit = True
+        )
+    def query_db(self,query,data=None):
         with self.connection.cursor() as cursor:
             try:
                 query = cursor.mogrify(query, data)
                 print("Running Query:", query)
-
-                executable = cursor.execute(query, data)
-                if query.lower().find("insert") >= 0:
-                    # if the query is an insert, return the id of the last row, since that is the row we just added
-                    self.connection.commit()
+                cursor.execute(query)
+                if query.lower().strip().startswith("select"):
+                    # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
+                    return cursor.fetchall()
+                elif query.lower().strip().startswith("insert"):
+                    # INSERT queries will return the ID NUMBER of the row inserted
                     return cursor.lastrowid
-                elif query.lower().find("select") >= 0:
-                    # if the query is a select, return everything that is fetched from the database
-                    # the result will be a list of dictionaries
-                    result = cursor.fetchall()
-                    return result
-                else:
-                    # if the query is not an insert or a select, such as an update or delete, commit the changes
-                    # return nothing
-                    self.connection.commit()
+                # UPDATE and DELETE queries will return nothing
             except Exception as e:
-                # in case the query fails
                 print("Something went wrong", e)
                 return False
             finally:
-                # close the connection
-                self.connection.close() 
-# this connectToMySQL function creates an instance of MySQLConnection, which will be used by server.py
-# connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
+                self.connection.close()
+
 def connectToMySQL(db):
     return MySQLConnection(db)
